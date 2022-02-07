47198963fb3e4c5a8a52fbba8fdc5b300e819930
farisqlail <faris.riskilail@gmail.com>
2022-02-07 18:34:43 +0700

    footer

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -618,7 +618,7 @@
   
           <header class="section-header">
             <h2>Portfolio</h2>
-            <p>Check our latest work</p>
+            <p>Our Work</p>
           </header>
   
           <div class="row" data-aos="fade-up" data-aos-delay="100">
@@ -1052,30 +1052,30 @@
                   <div class="info-box">
                     <i class="bi bi-geo-alt"></i>
                     <h3>Address</h3>
-                    <p>A108 Adam Street,<br>New York, NY 535022</p>
+                    <p>Indonesia, Surabaya</p>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="info-box">
                     <i class="bi bi-telephone"></i>
                     <h3>Call Us</h3>
-                    <p>+1 5589 55488 55<br>+1 6678 254445 41</p>
+                    <p>+62 878-7850-8854</p>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="info-box">
                     <i class="bi bi-envelope"></i>
                     <h3>Email Us</h3>
-                    <p>info@example.com<br>contact@example.com</p>
+                    <p>wedisdotco@gmail.com</p>
                   </div>
                 </div>
-                <div class="col-md-6">
+                {{-- <div class="col-md-6">
                   <div class="info-box">
                     <i class="bi bi-clock"></i>
                     <h3>Open Hours</h3>
                     <p>Monday - Friday<br>9:00AM - 05:00PM</p>
                   </div>
-                </div>
+                </div> --}}
               </div>
   
             </div>
