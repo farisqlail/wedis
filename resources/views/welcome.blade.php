279b256df02e85faabed41b7b6448e837a7076f9
farisqlail <faris.riskilail@gmail.com>
2022-10-27 20:03:57 +0700

    landing page done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -182,32 +182,26 @@
         </div>
     </section><!-- START: PRODUCTS -->
     <section class="products position-relative overflow-hidden">
-        <section class="right-circle">
-            <div class="d-flex justify-content-end">
-                <img src="https://api.elements.buildwithangga.com/storage/files/2/assets/Content/Content-workly/Right-Circle.png"
-                    alt="circle" />
-            </div>
-        </section>
-
         <section class="porto">
-            <div class="row d-flex align-items-center">
-                <div class="col-md-10">
-                    <h4 class="headline">
-                        Outputs from Our Platform
-                    </h4>
+            <div class="container">
+                <div class="row d-flex align-items-center">
+                    <div class="col-md-10">
+                        <h4 class="headline">
+                            Portfolio Kami
+                        </h4>
+                    </div>
+                    <div class="col-md-2 text-md-right">
+                        <a href="#" class="link h-100 d-inline-block text-decoration-none">
+                            Selengkapnya
+                        </a>
+                    </div>
                 </div>
-                <div class="col-md-2 text-md-right">
-                    <a href="#" class="link h-100 d-inline-block text-decoration-none">
-                        See more
-                    </a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <p class="sub-headline">
-                        Our platform specialize on helping our clients work process and communication. <br>
-                        Below are the products produced using our platform.
-                    </p>
+                <div class="row">
+                    <div class="col">
+                        <p class="sub-headline">
+                            Ini adalah beberapa portfolio yang kami miliki.
+                        </p>
+                    </div>
                 </div>
             </div>
             <div class="d-flex align-items-center justify-content-center gap-4 porto-img">
@@ -229,6 +223,69 @@
         </section>
     </section>
     <!-- END: PRODUCTS -->
+
+    <footer class="page-footer font-small blue">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+            crossorigin="anonymous" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet"
+            type="text/css" />
+
+        <div class="container text-md-left">
+            <div class="row">
+                <div class="col-md-6 mt-md-0 mt-3 address">
+                    <div class="logo font-red-hat-display">
+                        <a class="modal-title" id="targetModalLabel">
+                            <img style="margin-top: 0.5rem" src="@/assets/images/logo-putih.png" width="120px" />
+                        </a>
+                    </div>
+                    <div class="social-media">
+                        <a href="https://www.facebook.com/wedisdotco" target="blank">
+                            <img src="https://api.elements.buildwithangga.com/storage/files/2/assets/Content/Content10/dark/bi_facebook.svg"
+                                alt="facebook" class="img-fluid mr-4" />
+                        </a>
+                        <a href="https://www.facebook.com/wedisdotco" target="blank">
+                            <img src="https://api.elements.buildwithangga.com/storage/files/2/assets/Content/Content10/dark/bi_instagram.svg"
+                                alt="instagram" class="img-fluid mr-4" />
+                        </a>
+                    </div>
+                    <div class="copyright font-red-hat-display">
+                        2022 All rights reserved.
+                    </div>
+                </div>
+                <hr class="clearfix w-100 d-md-none pb-3" style="background-color: #2c3e57;" />
+                <div class="row col-md-6 nav-footer">
+                    <div class="col-md-6 mb-md-0 mb-3">
+                        <p>Fitur</p>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Portfolio</a>
+                            </li>
+                            <li>
+                                <a href="#!">Artikel</a>
+                            </li>
+                            <li>
+                                <a href="#!">Tentang Kami</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 mb-md-0 mb-3">
+                        <p>Kontak</p>
+                        <ul class="list-unstyled">
+                            <li>
+                                <span class="text-white">
+                                    <img src="@/assets/images/logo-wa.png" alt="wa" class="img-fluid "
+                                        width="50px" />
+                                    : +62 878-7850-8854</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    </template>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
     </script>
